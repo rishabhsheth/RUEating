2fe9944666a598d677183e84c0ec24edb42211ef
Krishna <84589322+kkg830@users.noreply.github.com>
2025-02-01 22:40:08 -0500

    Scraper Update 2

--- backend/scraping.py
+++ backend/scraping.py
@@ -1,40 +1,16 @@
-from bs4 import BeautifulSoup;
+from bs4 import BeautifulSoup
 import requests
+import datetime
 #https://menuportal23.dining.rutgers.edu/FoodPronet/pickmenu.aspx?locationNum=13&locationName=The+Atrium&dtdate=02/01/2025&activeMeal=Lunch&sName=Rutgers+University+Dining
 
-'''
-dininghall = input("Enter dining hall: (B) Busch, (L) Livingston, (N) Neilson, (A) Atrium")
-numstr = ''
-diningstr = ''
 
-if dininghall == 'B':
-    numstr = '04'
-    diningstr = 'Busch+Dining+Hall'
-elif dininghall == 'L':
-    numstr = '03'
-    diningstr = 'Livingston+Dining+Commons'
-elif dininghall == 'N':
-    numstr = '05'
-    diningstr = 'Neilson+Dining+Hall'
-elif dininghall == 'A':
-    numstr = '13'
-    diningstr = 'The+Atrium'
+base = datetime.datetime.today()
+dates = []
 
-url = 'https://menuportal23.dining.rutgers.edu/FoodPronet/pickmenu.aspx?locationNum='+numstr+'&locationName='+diningstr+'&dtdate=02/01/2025&activeMeal=Lunch&sName=Rutgers+University+Dining'
-page = requests.get(url)
 
-soup = BeautifulSoup(page.content,'html.parser')
-
-food_items = []
-fieldsets = soup.find_all('fieldset')
-for fieldset in fieldsets:
-    label = fieldset.find('label')
-    if label and label.has_attr('name'):
-        food_items.append(label["name"])
-
-for item in food_items:
-    print(item)
-'''
+for x in range(0, 7):
+    f_date = (base + datetime.timedelta(days=x)).strftime('%-m/%-d/%Y')
+    dates.append(f_date)
 
 fooddb = {
     'Busch': {'Breakfast':[],'Lunch':[],'Dinner':[]},
@@ -47,15 +23,15 @@ numstrs = ['04','03','05','13']
 mealstrs = ['Breakfast','Lunch','Dinner']
 campuses = ['Busch','Livvy','Neilson','Atrium']
 
-for x in range(4): #Dining Hall iteration
-    for y in range(3): #Meal Iteration
-        url = 'https://menuportal23.dining.rutgers.edu/FoodPronet/pickmenu.aspx?locationNum='+numstrs[x]+'&locationName='+diningstrs[x]+'&dtdate=02/01/2025&activeMeal='+mealstrs[y]+'&sName=Rutgers+University+Dining'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content,'html.parser')
-        fieldsets = soup.find_all('fieldset')
-        for fieldset in fieldsets:
-            label = fieldset.find('label')
-            if(label and label.has_attr('name')):
-                fooddb[campuses[x]][mealstrs[y]].append(label['name'])
-
+for w in range(7): #Days Iteration
+    for x in range(4): #Dining Hall Iteration
+        for y in range(3): #Meal Iteration
+            url = 'https://menuportal23.dining.rutgers.edu/FoodPronet/pickmenu.aspx?locationNum='+numstrs[x]+'&locationName='+diningstrs[x]+'&dtdate='+dates[w]+'&activeMeal='+mealstrs[y]+'&sName=Rutgers+University+Dining'
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content,'html.parser')
+            fieldsets = soup.find_all('fieldset')
+            for fieldset in fieldsets:
+                label = fieldset.find('label')
+                if(label and label.has_attr('name')):
+                    fooddb[campuses[x]][mealstrs[y]].append(label['name'])
 print(fooddb)
