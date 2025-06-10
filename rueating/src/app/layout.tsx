cf85934ff448297804b22669f57cf8bf0493f199
Rishabh Sheth <shethrishabh1@gmail.com>
2025-06-10 10:30:23 -0400

    added analytics hopefully

--- rueating/src/app/layout.tsx
+++ rueating/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,18 +20,38 @@ export const metadata: Metadata = {
   description: "HackRU Project",
 };
 
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
 }
+
