499f05e613edf7dfa628ae4e7d84e91cd2f5ea13
Rohit Bhagat <132160385+rb0125@users.noreply.github.com>
2025-07-28 18:13:11 -0400

    Fixed port number error

--- rueating/src/app/api/route.ts
+++ rueating/src/app/api/route.ts
@@ -6,7 +6,7 @@ async function fetchData (data: string) : Promise<string[][]> {
     // Connect to DB
     const sql = postgres({
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT),
       database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWD,
