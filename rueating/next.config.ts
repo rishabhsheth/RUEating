0b2588a720b78690cb199f8e91229ff28b798aa4
Rohit Bhagat <132160385+rb0125@users.noreply.github.com>
2025-08-20 20:51:39 -0400

    First commit: Python serverless function test

--- rueating/next.config.ts
+++ rueating/next.config.ts
@@ -1,18 +1,35 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-// export default nextConfig;
+// // export default nextConfig;
 
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
-//       },
-//     ]
-//   },
-// }
+// // module.exports = {
+// //   async rewrites() {
+// //     return [
+// //       {
+// //         source: '/api/:path*',
+// //         destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+// //       },
+// //     ]
+// //   },
+// // }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
